d1faf5d7fe9a2d43b0f78e3b6bb6951a3bb7b11e
Theinterna <yalvin58@gmail.com>
2024-07-01 16:57:19 -0700

    Add offset variable for better camera positioning

--- Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+    // add an offset so that the camera is above and behind vehicle
+    // we need the new keyword to add the "new" Vector3 offset
+    // we can now edit this offset in inspector :)
+    public Vector3 offset = new Vector3(0, 8, -7);
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,6 @@ public class FollowPlayer : MonoBehaviour
     void Update()
     {
         // sets camera's position to player's position
-        transform.position = player.transform.position;
+        transform.position = player.transform.position + offset;
     }
 }
