1e0681cf17095b90be5d271e0da68b8d1a907d38
Theinterna <yalvin58@gmail.com>
2024-07-01 16:14:47 -0700

    Adjust speed to be based on seconds
    Specifically, 20 meters per second

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,9 @@ public class PlayerController : MonoBehaviour
     {
         // Move the vehicle forward
         // We could technically also use 0,0,1, but Vector3.forward is more readable
-        transform.Translate(Vector3.forward);
+        // We use Time.deltaTime because Update is once per frame, but that depends on device
+        // So, we want to do move based on time (i.e. seconds)
+        // The code below is 0,0,1 per second, * 20, so 20 meters per second :)
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
