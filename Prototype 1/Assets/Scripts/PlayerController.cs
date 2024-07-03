f796555d9a2a912a1014b2ec035283b3cef0613d
Theinterna <yalvin58@gmail.com>
2024-07-02 23:35:17 -0700

    Add player controls
    This is so that player can turn left and right.
    Mass of vehicle is changed for better barreling.

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20.0f;
+    public float turnSpeed = 5.0f;
+    public float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +16,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         // Move the vehicle forward
         // We could technically also use 0,0,1, but Vector3.forward is more readable
         // We use Time.deltaTime because Update is once per frame, but that depends on device
         // So, we want to do move based on time (i.e. seconds)
         // The code below is 0,0,1 per second, * 20, so 20 meters per second :)
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // if turnSpeed is negative, the car goes along left axis (car will face forward)
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
