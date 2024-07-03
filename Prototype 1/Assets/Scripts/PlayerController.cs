74c8baceebfa6d2997ed30fc665bdee9348eebef
Theinterna <yalvin58@gmail.com>
2024-07-02 23:41:25 -0700

    Change vehicle type
    Other vehicle had glitch in collider that caused barrels to go through the tank.

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 20.0f;
     public float turnSpeed = 5.0f;
     public float horizontalInput;
+    public float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +18,13 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // Move the vehicle forward
         // We could technically also use 0,0,1, but Vector3.forward is more readable
         // We use Time.deltaTime because Update is once per frame, but that depends on device
         // So, we want to do move based on time (i.e. seconds)
         // The code below is 0,0,1 per second, * 20, so 20 meters per second :)
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // if turnSpeed is negative, the car goes along left axis (car will face forward)
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
