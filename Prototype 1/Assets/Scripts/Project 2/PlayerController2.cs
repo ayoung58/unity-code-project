b3390e02055ffb4296f5dc21c9a839a8252ed258
Theinterna <yalvin58@gmail.com>
2024-07-06 11:03:46 -0700

    Allow player movement left and right

---
+++ Prototype 1/Assets/Scripts/Project 2/PlayerController2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    private float horizontalInput;
+    public float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(horizontalInput * Time.deltaTime * Vector3.right * speed);
+    }
+}
