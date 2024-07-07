939151170dabbee51fdc48c2d9e5d517ddc8e632
Theinterna <yalvin58@gmail.com>
2024-07-07 02:01:01 -0700

    Adjust when animal disappears

--- Prototype 1/Assets/Scripts/Project 2/DestroyOutOfBounds2.cs
+++ Prototype 1/Assets/Scripts/Project 2/DestroyOutOfBounds2.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBounds2 : MonoBehaviour
 {
     private float topBound = 30;
-    private float lowerBound = -10;
+    private float lowerBound = -3;
     // Start is called before the first frame update
     void Start()
     {
