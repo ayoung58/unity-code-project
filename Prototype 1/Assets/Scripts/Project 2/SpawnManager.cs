b12c5b6637408d4481238aa7290624c564ab0857
Theinterna <yalvin58@gmail.com>
2024-07-06 23:39:45 -0700

    Randomize spawn location of animals

--- Prototype 1/Assets/Scripts/Project 2/SpawnManager.cs
+++ Prototype 1/Assets/Scripts/Project 2/SpawnManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,11 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // choose a random index, basically choosing a random animal
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ;
+        if (Input.GetKeyDown(KeyCode.S)) {
+            Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
