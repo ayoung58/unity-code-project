7f7031dd6a4cace12b9ff9b17cbc02531c4636b7
Theinterna <yalvin58@gmail.com>
2024-07-08 15:24:44 -0700

    Spawn different colored balls randomly

--- Prototype 1/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Prototype 1/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -25,8 +25,9 @@ public class SpawnManagerX : MonoBehaviour
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
+        GameObject randomBall = ballPrefabs[Random.Range(0, ballPrefabs.Length)];
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(randomBall, spawnPos, randomBall.transform.rotation);
     }
 
 }
