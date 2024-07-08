6281fa7e7df622f9512d5d9ca5ff3d26b19220ae
Theinterna <yalvin58@gmail.com>
2024-07-08 15:12:27 -0700

    Resolve name discrepancy
    There is already a PlayerControllerX from a previous challenge, so this script needs to be changed to PlayerControllerX2

--- Prototype 1/Assets/Challenge 2/Scripts/PlayerControllerX2.cs
+++ Prototype 1/Assets/Challenge 2/Scripts/PlayerControllerX2.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControllerX : MonoBehaviour
+public class PlayerControllerX2 : MonoBehaviour
 {
     public GameObject dogPrefab;
 
